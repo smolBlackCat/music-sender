f2102929265a3c5bee5945ebdd581007c1120454
moura <m0ur4@protonmail.com>
2022-09-04 11:13:38 -0300

    add: Basic package setup created.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="music-sender",
+    version="v1.0",
+    description="music-sender is a project that aims to easy the process of "
+        "transferring files between computers through terminal.",
+    author="De Moura",
+    author_email="m0ur4@protonmail.com",
+    packages=["music_sender"]
+)
