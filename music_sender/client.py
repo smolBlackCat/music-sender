345a6b097836945edc658ab1cb39aafed06b1497
moura <m0ur4@protonmail.com>
2022-09-14 08:39:39 -0300

    fix: Each client request function uses a new socket.

--- music_sender/client.py
+++ music_sender/client.py
@@ -12,13 +12,17 @@ class MusicSenderClient(Communicator):
     def __init__(self, address: tuple[str, int]):
         super().__init__()
         self.address = address
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = None
 
-        self.sock.connect(address)
-    
-    def close(self):
-        self.sock.close()
+    def _connection_decorator(f):
 
+        def connection(self, *args, **kwargs):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.sock:
+                self.sock.connect(self.address)
+                return f(self, *args, **kwargs)
+        return connection
+
+    @_connection_decorator
     def songs_list(self) -> list[tuple[int, str]]:
         """Makes a 'list' request to the server.
 
@@ -40,9 +44,10 @@ class MusicSenderClient(Communicator):
         songs = self.songs_list()
         return list(filter(lambda song: song[1] not in os.listdir("."), songs))
 
+    @_connection_decorator
     def request_song(self, index: int):
         """Makes a 'request <index>' request to the server.
-        
+
         Args:
             index: The requested index from which the song comes from.
         """
@@ -53,7 +58,7 @@ class MusicSenderClient(Communicator):
 
 def songs_list_out(songs_list: list[tuple[int, str]]) -> None:
     """Prints out to the user the list of musics.
-    
+
     Args:
         songs_list: list of tuples containing an int and a str, that
                     is, the song's index and the song's name
@@ -78,7 +83,6 @@ def main():
     songs_list_out(client.missing_songs_list())
     client.request_song(1)
 
-    client.close()
 
 if __name__ == "__main__":
     main()
